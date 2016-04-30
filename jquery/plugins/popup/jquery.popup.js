fc6573fb1b0a98ac59fd01a2f4689430d5eead57
Erwin Gaitán Ospino <erwingaitano@gmail.com>
2016-04-30 02:38:08 -0500

    updating popup plugin

--- jquery/plugins/popup/jquery.popup.js
+++ jquery/plugins/popup/jquery.popup.js
@@ -1,91 +1,75 @@
+/* eslint prefer-arrow-callback: 0, prefer-template: 0, no-var: 0, object-shorthand: 0 */
+/* global define */
+
 /*
   Init the popup plugin:
-    You should assing the plugin to a global namespace different from popup and it should be initialize only once. Example:
+    You should assing the plugin to a global namespace different from popup and
+    it should be initialize only once. Example:
 
     window.popupManager = window.popupManager || new window.popup();
 
   How to use:
     Basic:
       .popup-tampax.js-popup-tampax
-        .popup.js-popup{:'data-popup' => 'my-popup'}
-          %a.js-close-popup x
+        .popup.js-popup(data-popup='my-popup')
+          a.js-close-popup x
 
       Link that calls the popup:
-      %a.js-open-popup{:'data-popup' => 'my-popup'} Open Popup
+      a.js-open-popup(data-popup='my-popup') Open Popup
 
     Ajax:
       .popup-tampax.js-popup-tampax
-        .popup.js-popup{:'data-popup' => 'my-ajaxpopup', :'data-popup-container-class' => 'js-popup-data'}
-          %a.js-close-popup x
+        .popup.js-popup(data-popup='my-ajaxpopup', data-popup-container-class='js-popup-data')
+          a.js-close-popup x
           .js-popup-data
 
       Link that calls the popup:
-      %a.js-open-popup{:'data-popup' => 'my-ajaxpopup', :'data-popup-ajax-url' => '/privacy_ajax'} Open Popup
+      a.js-open-popup(data-popup='my-ajaxpopup', data-popup-ajax-url='/privacy_ajax') Open Popup
 
   NOTES::
     IT must always be 1 container (.popup-tampax) to contain all the popups. Example:
 
     This is OK:
-      .popup-tampax.js-popup-tampax
-        .popup.js-popup{:'data-popup' => 'my-popup'}
-          %a.js-close-popup x
-        .popup.js-popup{:'data-popup' => 'my-popup2'}
-          %a.js-close-popup x
-        .popup.js-popup{:'data-popup' => 'my-popup3'}
-          %a.js-close-popup x
+      body
+        .popup-tampax.js-popup-tampax
+          .popup.js-popup(data-popup='my-popup')
+            a.js-close-popup x
+          .popup.js-popup(data-popup='my-popup2')
+            a.js-close-popup x
+          .popup.js-popup(data-popup='my-popup3')
+            a.js-close-popup x
 
     This is NOT OK:
-      .popup-tampax.js-popup-tampax
-        .popup.js-popup{:'data-popup' => 'my-popup'}
-          %a.js-close-popup x
+      body
+        .popup-tampax.js-popup-tampax
+          .popup.js-popup(data-popup='my-popup')
+            a.js-close-popup x
 
-      .popup-tampax.js-popup-tampax
-        .popup.js-popup{:'data-popup' => 'my-popup2'}
-          %a.js-close-popup x
+        .popup-tampax.js-popup-tampax
+          .popup.js-popup(data-popup='my-popup2')
+            a.js-close-popup x
 
-      .popup-tampax.js-popup-tampax
-        .popup.js-popup{:'data-popup' => 'my-popup3'}
-          %a.js-close-popup x
+        .popup-tampax.js-popup-tampax
+          .popup.js-popup(data-popup='my-popup3')
+            a.js-close-popup x
 
 */
 
-
-// Uses CommonJS, AMD or browser globals to create a jQuery plugin.
-
-(function (factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['jquery'], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        // Node/CommonJS
-        module.exports = function( root, jQuery ) {
-            if ( jQuery === undefined ) {
-                // require('jQuery') returns a factory that requires window to
-                // build a jQuery instance, we normalize how we use modules
-                // that require this pattern but the window provided is a noop
-                // if it's defined (how jquery works)
-                if ( typeof window !== 'undefined' ) {
-                    jQuery = require('jquery');
-                }
-                else {
-                    jQuery = require('jquery')(root);
-                }
-            }
-            factory(jQuery);
-            return jQuery;
-        };
-    } else {
-        // Browser globals
-        factory(jQuery);
-    }
-}(function ($) {
-  // Namespacing
+(function init(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['jquery'], factory);
+  } else if (typeof exports === 'object') {
+    // Node, CommonJS-like
+    module.exports = factory(require('jquery'));
+  }
+}(this, function init($) {
   var eventListeners = [];
   var pluginName = 'popup';
 
   var defaults = {
-    onBeforeClose: function () { return true; },
-    onBeforeOpen: function () { return true; },
+    onBeforeClose: function init() { return true; },
+    onBeforeOpen: function init() { return true; },
     animationDelayOnClose: 600,
     focusInputInside: true,
     container: '.js-popup-tampax',
@@ -104,14 +88,15 @@
     return this;
   }
 
-  Popup.prototype = {
-    init: function () {
-      var _this = this;
+  function fireEventListeners(name, popup, btn) {
+    eventListeners.forEach(function init(el) {
+      if (el.name === name) el.callback(popup, popup.data('popup'), btn);
+    });
+  }
 
-      this.$container.off('.' + pluginName);
-      this.$container.on('click.' + pluginName + ' touchstart.' + pluginName, onTampaxClick);
-      $(this.settings.openBtn).bind('click.' + pluginName, onBtnOpen);
-      $(this.settings.closeBtn).bind('click.' + pluginName, this.closePopups.bind(this));
+  Popup.prototype = {
+    init: function init() {
+      var _this = this;
 
       function onTampaxClick(e) {
         if ($(e.target).closest(_this.$popups).length) return;
@@ -125,7 +110,19 @@
         var $this = $(this);
         var popupName = $this.data('popup');
 
-        $popup = _this.$popups.filter(function (i, el) {
+        function onSuccess(data) {
+          $popupContainer.html(data);
+          $popup.removeClass('is-loading');
+          fireEventListeners('onAjaxSucess', $popup);
+        }
+
+        function onError(data) {
+          $popupContainer.html(data);
+          $popup.removeClass('is-loading');
+          fireEventListeners('onAjaxError', $popup);
+        }
+
+        $popup = _this.$popups.filter(function init(i, el) {
           return el.attributes['data-popup'].value === popupName;
         });
 
@@ -144,25 +141,20 @@
             $.get($this.data('popup-ajax-url')).then(onSuccess, onError);
           }
         }
+      }
 
-        function onSuccess(data) {
-          $container.html(data);
-          popup.removeClass('is-loading');
-          fireEventListeners('onAjaxSucess', $popup);
-        }
-
-        function onError(data) {
-          $container.html(data);
-          popup.removeClass('is-loading');
-          fireEventListeners('onAjaxError', $popup);
-        }
-      };
+      this.$container.off('.' + pluginName);
+      this.$container.on('click.' + pluginName + ' touchstart.' + pluginName, onTampaxClick);
+      $(this.settings.openBtn).bind('click.' + pluginName, onBtnOpen);
+      $(this.settings.closeBtn).bind('click.' + pluginName, this.closePopups.bind(this));
     },
 
-    openPopup: function (popup, btn, event) {
+    openPopup: function init(popup, btn, event) {
       var _this = this;
+      var shouldOpen;
+
       popup = typeof popup === 'string' ?
-        this.$popups.filter(function (i, el) {
+        this.$popups.filter(function init(i, el) {
           return el.attributes['data-popup'].value === popup;
         }) : $(popup);
 
@@ -170,11 +162,12 @@
       // haven't been downloaded so we have to make sure they exists)
       // fix the event.preventdefault when this is executed after a click event
       if (popup.length) {
+        shouldOpen = this.settings.onBeforeOpen(popup, btn);
+
         if (event && event.stopPropagation) event.stopPropagation();
         this.$container.addClass('is-opened');
 
         // If onBefore returns false, we don't close the popup
-        var shouldOpen = this.settings.onBeforeOpen(popup, btn);
         fireEventListeners('onBeforeOpen', popup, btn);
         if (!shouldOpen) return;
 
@@ -182,7 +175,7 @@
         popup.addClass('is-active');
         fireEventListeners('onOpen', popup, btn);
         window.clearTimeout(this.closePopupsTimeout);
-        return this.openPopupTimeout = window.setTimeout(function () {
+        this._openPopupTimeout = window.setTimeout(function init() {
           if (_this.settings.focusInputInside) {
             popup.find('input:first').focus();
           }
@@ -190,7 +183,7 @@
       }
     },
 
-    closePopups: function () {
+    closePopups: function init() {
       var _this = this;
       var activePopup = this.getActivePopup();
 
@@ -199,36 +192,30 @@
       fireEventListeners('onBeforeClose', activePopup);
       if (!shouldClose) return;
 
-      window.clearTimeout(this.openPopupTimeout);
-      this.closePopupsTimeout = window.setTimeout(function () {
+      window.clearTimeout(this._openPopupTimeout);
+      this.closePopupsTimeout = window.setTimeout(function init() {
         _this.$popups.removeClass('is-active');
       }, this.settings.animationDelayOnClose);
       this.$container.removeClass('is-opened');
       fireEventListeners('onClose', _this.getActivePopup());
     },
 
-    getActivePopup: function () {
-      return this.$popups.filter(function (i, el) {
+    getActivePopup: function init() {
+      return this.$popups.filter(function init(i, el) {
         return $(el).hasClass('is-active');
       });
     },
 
-    addEventListener: function (name, callback) {
+    addEventListener: function init(name, callback) {
       eventListeners.push({ name: name, callback: callback });
     },
 
-    destroy: function () {
+    destroy: function init() {
       this.$container.off('.' + pluginName + ' touchstart.' + pluginName);
       $(this.settings.openBtn).unbind('click.' + pluginName);
       $(this.settings.closeBtn).unbind('click.' + pluginName);
     }
   };
-
-  function fireEventListeners (name, popup, btn) {
-    eventListeners.forEach(function (el) {
-      if (el.name === name) el.callback(popup, popup.data('popup'), btn);
-    });
-  }
 
   return Popup;
 }));
--- js/error-creator.js
+++ js/error-creator.js
@@ -4,39 +4,45 @@
 (function init(root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
+    define(['jquery', 'underscore'], factory);
+  } else if (typeof exports === 'object') {
+    // Node, CommonJS-like
+    module.exports = factory(require('jquery'), require('underscore'));
+  } else {
+    // Browser globals (root is window)
+    // If you don't want to export to the window object, you
+    // can remove this else statement
+    root.moduleExports = factory(root.$, root._);
+  }
+}(this, function init($, _) {
+  console.log('do something with: ', $, _);
+
+  // Just return a value to define the module export.
+  // This example returns an object, but the module
+  // can return a function as the exported value.
+  return {};
+}));
+
+
+/* eslint prefer-arrow-callback: 0 */
+/* global define */
+
+(function init(root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
     define([], factory);
   } else if (typeof module === 'object' && module.exports) {
-    // Node. Does not work with strict CommonJS, but
-    // only CommonJS-like environments that support module.exports,
-    // like Node.
+    // Node, CommonJS-like
     module.exports = factory();
   } else {
-    // Browser globals(root is window)
-    root.returnExports = factory();
+    // Browser globals (root is window)
+    // If you don't want to export to the window object, you
+    // can remove this else statement
+    root.moduleExports = factory();
   }
-}(this, function init() {
-  /**
-   * To create a custom error, you should call:
-   *    const MyError = errorCreator('MyError');`
-   * Now you can normally create custom errors:
-   *    const err = new MyError('error', { info: 'you da man' });
-   * Note that you can actually pass an object with additional data!
-   *
-   * @param  {string}   titleCasedName  titlecased name of your custom error. Eg 'MyError'
-   * @return {function} creator         The constructor for your custom error
-   */
-
-  return function errorCreator(titleCasedName) {
-    function creator(message, data = {}) {
-      this.name = titleCasedName;
-      this.message = message;
-      this.data = data;
-      Error.captureStackTrace(this, creator);
-    }
-
-    creator.prototype = Object.create(Error.prototype);
-    creator.prototype.constructor = creator;
-
-    return creator;
-  };
+}(this, function define() {
+  // Just return a value to define the module export.
+  // This example returns an object, but the module
+  // can return a function as the exported value.
+  return {};
 }));
